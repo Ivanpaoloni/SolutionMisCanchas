6a8fb626b6ecec1049dba23d21f38c0827151b97
ivanpaoloni@gmail.com <ivanpaoloni@gmail.com>
2024-05-04 19:41:35 -0300

    categoryIcons

--- Crud.Domain/Entities/MovementType.cs
+++ Crud.Domain/Entities/MovementType.cs
@@ -9,8 +9,8 @@ namespace MisCanchas.Domain.Entities
     public class MovementType
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Movement> Movements { get; set; }
+        public string? Name { get; set; }
+        public List<Movement>? Movements { get; set; }
         public bool Incremental { get; set; }
     }
 }
---
+++ MisCanchas.EfSqlRepository/Migrations/20240504220347_movementTypeUpdate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MisCanchas.EfSqlRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class movementTypeUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "MovementTypes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "MovementTypes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
